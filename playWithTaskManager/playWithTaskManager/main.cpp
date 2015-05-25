764b82f242894ee3327e2b126caa8bbacdf5a91a
Mincong Zhang <mincong.zhang@hotmail.com>
2015-05-25 20:48:57 +0100

    sin wave done

--- playWithTaskManager/playWithTaskManager/main.cpp
+++ playWithTaskManager/playWithTaskManager/main.cpp
@@ -1,19 +1,36 @@
 #include <Windows.h>  
 #include<stdlib.h>  
+#include<math.h>  
 #include<tchar.h>  
-int _tmain(int argc,_TCHAR* argv[])  
+const double SPLIT = 0.01;  
+const int COUNT = 200;  
+const double PI = 3.14159265;  
+const int INTERVAL = 300;  
+int _tmain(int argc, _TCHAR* argv[] )  
 {  
-    int busyTime = 2;  
-    int idleTime = busyTime;  
-    __int64 startTime = 0;  
-    ::SetProcessAffinityMask(::GetCurrentProcess(),0x00000001);  
-    while(true)  
+    DWORD busySpan[COUNT]; //array of busy times  
+    DWORD idleSpan[COUNT]; //array of idle times  
+    int half = INTERVAL/2;  
+    double radian = 0.0;  
+    for(int i = 0; i < COUNT; ++i)  
     {  
+        busySpan[i] = (DWORD)(half + (sin(PI * radian) * half));  
+        idleSpan[i] = INTERVAL - busySpan[i];  
+        radian += SPLIT;  
+    }  
+    DWORD startTime = 0;  
+    int j = 0;  
+
+	//Specify process to avoid process shift. 
+    ::SetProcessAffinityMask(::GetCurrentProcess(),0x00000002);  
+    
+	while(true)  
+    {  
+        j = j % COUNT;  
         startTime = GetTickCount();  
-                //busy loop  
-        while((GetTickCount() - startTime) <= busyTime);  
-                //idle loop  
-            Sleep(idleTime);  
+        while((GetTickCount() - startTime) <= busySpan[j]);  
+            Sleep(idleSpan[j]);  
+        j++;  
     }  
     return 0;  
 }
